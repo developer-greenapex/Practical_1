205b8ed4e6548e74d7b84ee161708cc524cdc77c
Soumya Sau <59736884+Soumya47@users.noreply.github.com>
2022-05-18 14:16:22 +0530

    initial

---
+++ src/main/java/com/greenapex/CSV_Reader/CsvReaderApplication.java
@@ -0,0 +1,13 @@
+package com.greenapex.CSV_Reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenapex/CSV_Reader/CsvReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenapex.CSV_Reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
