a4a8bfeb8b702e750738d42c24ef48417fb6c069
Pankaj Mandal <pankaj.mandal@green-apex.com>
2022-05-18 16:02:10 +0530

    project structure

--- src/main/java/com/greenapex/CSV_Reader/CsvReaderApplication.java
+++ src/main/java/com/greenapex/CSV_Reader/CsvReaderApplication.java
@@ -2,8 +2,10 @@ package com.greenapex.CSV_Reader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class CsvReaderApplication {
 
 	public static void main(String[] args) {
